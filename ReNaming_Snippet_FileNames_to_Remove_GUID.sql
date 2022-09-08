04c362ae0ecb5d94922dfeaef8497c1402141221
ChandraSaiKarthik <chandrasaikarthik@gmail.com>
2022-09-08 10:05:30 +0530

    Comments in ReNaming_Snippet_FileNames_to_Remove_GUID

--- ReNaming_Snippet_FileNames_to_Remove_GUID.sql
+++ ReNaming_Snippet_FileNames_to_Remove_GUID.sql
@@ -1,40 +1,37 @@
-/* =====================================================================================================================
--- Author		: SaiKarthik
--- Create date	: 25-10-2020
+/* =============================================================================================================================================================
 -- Database		: USE master
 -- Script		: Trimming(ReName) Snippet FileNames to Remove GUID.sql
 -- Description	: This Script Rename the ".json" Files in the Snippets Folder which are being AutoRenamed by SQLprompt.
 --				  So, To Rename the ".json" Files to Default File Names by Removing the PostFixed GUID in the SnippetName.
 -- Usage		: Change the @Folder_Path where ".json" files are stored.
-===================================================================================================================== */
+============================================================================================================================================================= */
 USE master;
 GO
 
 --
 DECLARE @Folder_Path NVARCHAR(4000) = N'C:\GitHub\SQLpromptSnippets\Snippets';
-
-/**********************************************************************************************************************/
-/* DONOT CHANGE FROM BELOW ********************************************************************************************/
-/**********************************************************************************************************************/
+/**************************************************************************************************************************************************************/
+/* ===== ** DO NOT CHANGE CODE BELOW FROM HERE ** =========================================================================================================== */
+/**************************************************************************************************************************************************************/
 SET NOCOUNT ON;
 SET XACT_ABORT ON;
 
 --
-/* Deleting the "FileList.txt" in the FolderPath "@Folder_Path" If Exists *********************************************/
+/* ===== ** Deleting the "FileList.txt" in the FolderPath "@Folder_Path" If Exists ** ======================================================================= */
 DECLARE @DEL_FileList NVARCHAR(4000);
 SET @DEL_FileList = N'del /S "' + @Folder_Path + N'\FileList.txt"';
 --PRINT @DEL_FileList;
 EXEC sys.xp_cmdshell @command = @DEL_FileList;
 
 --
-/* Creating the New "FileList.txt" in the FolderPath "@Folder_Path" ***************************************************/
+/* ===== ** Creating the New "FileList.txt" in the FolderPath "@Folder_Path" ** ============================================================================= */
 DECLARE @MK_FileList NVARCHAR(4000);
 SET @MK_FileList = N'for /F "tokens=*" %a in (''dir "' + @Folder_Path + N'\" /s /b'') do echo %~fa >>"' + @Folder_Path + N'\FileList.txt"';
 --PRINT @MK_FileList;
 EXEC sys.xp_cmdshell @command = @MK_FileList;
 
 --
-/* Creating "#Temp_SnippetList" Table for Raw Data of "FileList.txt" *****************************************************/
+/* ===== ** Creating "#Temp_SnippetList" Table for Raw Data of "FileList.txt" ** ============================================================================ */
 IF OBJECT_ID('tempdb..#Temp_SnippetList', 'U') IS NOT NULL
 	DROP TABLE #Temp_SnippetList;
 
@@ -43,7 +40,7 @@ CREATE TABLE #Temp_SnippetList
 	SnippetName VARCHAR(4000) NOT NULL
 ) ON [PRIMARY];
 
-/* Inserting "FileList.txt" Data into "#Temp_SnippetList" Table **********************************************************/
+/* ===== ** Inserting "FileList.txt" Data into "#Temp_SnippetList" Table ** ================================================================================= */
 DECLARE @SnippetListImportString NVARCHAR(4000);
 SET @SnippetListImportString = N'BULK INSERT #Temp_SnippetList FROM ''' + @Folder_Path + N'\FileList.txt'' WITH (FIELDTERMINATOR  = '','', FIRSTROW = 1)';
 --PRINT @SnippetListImportString;
@@ -51,7 +48,7 @@ EXEC sys.sp_executesql @command = @SnippetListImportString;
 
 SELECT SnippetName FROM #Temp_SnippetList;
 
-/* Creating the Table "#Final_SnippetList" for Renaming the Files Accordingly ********************************************/
+/* ===== ** Creating the Table "#Final_SnippetList" for Renaming the Files Accordingly ** =================================================================== */
 IF OBJECT_ID('tempdb..#Final_SnippetList', 'U') IS NOT NULL
 	DROP TABLE #Final_SnippetList;
 
@@ -70,8 +67,8 @@ FROM _CTE AS CTE;
 SELECT FFL.Id, FFL.Old_SnippetName, FFL.New_SnippetName
 FROM #Final_SnippetList AS FFL;
 
-/**********************************************************************************************************************/
-/* Renaming the Files Only if NewSnippetName != ".json" ******************************************************************/
+/**************************************************************************************************************************************************************/
+/* ===== ** Renaming the Files Only if NewSnippetName != ".json" ** ========================================================================================= */
 IF EXISTS (SELECT * FROM #Final_SnippetList WHERE New_SnippetName <> '.json')
 BEGIN
 	BEGIN TRANSACTION;
@@ -122,5 +119,5 @@ BEGIN
 	EXEC sys.xp_cmdshell @command = 'pause';
 END;
 --
-/*Deleting the Flie "FileList.txt" Again which is Created for this Process ********************************************/
+/* ===== ** Deleting the Flie "FileList.txt" Again which is Created for this Process ** ===================================================================== */
 EXEC sys.xp_cmdshell @command = @DEL_FileList;
